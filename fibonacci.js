7b85e43db4166ff5884a853f4c2be56a23d9bfb4
annascriv <scriven.anna@outlook.com>
2023-10-04 16:56:02 -0500

    returns number at nth index

--- fibonacci.js
+++ fibonacci.js
@@ -1,5 +1,21 @@
 function fibonacci(num) {
-  return 0;
-}
+  let fibs = [0,1];
 
+  if (num==0) {
+    return 0;
+  }
+  if (num==1) {
+    return 1;
+   }
+  
+
+  for (let i=2;i<=num;i++) {
+   // fibs[i] = fibs[i-2]+fibs[i-1];
+    fibs.push(fibs[i-2]+fibs[i-1]);
+
+  }
+  console.log(fibs)
+  return fibs[num];
+}
+console.log(fibonacci(11))
 module.exports = fibonacci;
